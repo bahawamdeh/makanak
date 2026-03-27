7ea50932969ee057fd1493a570452f23f248d5e4
Bashar <abe.melad@gmail.com>
2026-03-27 21:14:18 +0200

    add test line

--- makanak-backend/src/config/db.js
+++ makanak-backend/src/config/db.js
@@ -2,20 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // إعدادات الاتصال بـ MongoDB
-    const options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true, // لتجنب التحذيرات من Mongoose
-      useFindAndModify: false,
-    };
+    await mongoose.connect(process.env.MONGO_URI);
 
-    await mongoose.connect(process.env.MONGO_URI, options);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    // إخراج خطأ والمغادرة مع رمز 1 للإشارة إلى أن العملية فشلت
-    process.exit(1);
+    process.exit(1); // ❗ لازم يكون داخل catch
   }
 };
 
